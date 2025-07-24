4c9b886df47e6572d57311f0e3897cc974a26a38
vipin joshi <vpnjsh55@gmail.com>
2025-07-24 12:50:02 +0530

    process watcher implementation structs and analyze and check

--- src/core/process_watcher.rs
+++ src/core/process_watcher.rs
@@ -1,7 +1,11 @@
+use core::f64;
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::error::Error;
 use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
-use chrono::Duration;
+use sysinfo::{Pid, Process, System};
 
 pub struct ProcessInfo {
     process_name: String,
@@ -73,7 +77,7 @@ impl ResourceMonitor {
                 let avg_cpu: f32 = cpu_item.iter().sum::<f32>() / cpu_item.len() as f32;
                 if avg_cpu > self.cpu_threshold {
                     warnings.push(format!(
-                        "Average cpu usage {:.2}% is higher than recommended one {:.2}%",
+                        "Average cpu usage :: {:.2}% is higher than recommended :: {:.2}%",
                         avg_cpu, self.cpu_threshold
                     ))
                 }
@@ -94,16 +98,70 @@ impl ResourceMonitor {
 
 pub struct ProcessMonitor {
     scan_interval: Duration,
-    //resouce_monitor
+    resource_monitor: ResourceMonitor,
+    system: System,
+    previous_processes: HashMap<u32, ProcessInfo>,
 }
 
 impl ProcessMonitor {
-    pub fn new(interval: Duration, cpu_threshold: f32, mem_threshold: f64) -> Self {
+    pub fn new(interval: Duration, cpu_threshold: f32, mem_threshold: f64, bound: usize) -> Self {
         ProcessMonitor {
             scan_interval: interval,
-            //resource_monitor::new(cpu_th, mem_th)
+            resource_monitor: ResourceMonitor::new(cpu_threshold, mem_threshold, bound),
+            system: System::new_all(),
+            previous_processes: HashMap::new(),
         }
     }
 
-    // pub fn watch_processes(&self) {}
+    pub fn get_process_info(&self, pid: &Pid, process: &Process) -> ProcessInfo {
+        ProcessInfo {
+            process_name: format!("{:?}", process.name()),
+            pid: pid.as_u32(),
+            cpu_usage: process.cpu_usage(),
+            mem_usage: process.memory() as f64,
+            exc_path: process.exe().map(|item| item.to_path_buf()),
+        }
+    }
+
+    pub fn check_process(&mut self, process_info: &ProcessInfo) {
+        //check or analyze the process for threats and memory and cpu usages
+    }
+
+    pub fn scan_processes(&mut self) -> Result<(), Box<dyn Error>> {
+        let mut current_process: HashMap<u32, ProcessInfo> = HashMap::new();
+        let mut current_pids: HashSet<u32> = HashSet::new();
+        //scan all the processes
+        self.system.refresh_all();
+        for (pid, process) in self.system.processes() {
+            let process_info = self.get_process_info(pid, process);
+            current_pids.insert(process_info.pid);
+            current_process.insert(process_info.pid, process_info);
+        }
+
+        for process_info in current_process.values() {
+            self.check_process(process_info);
+        }
+        //detect new processes
+        // self.detect_new_process(&current_process);
+        //update the collections
+        //clear the collection
+
+        self.previous_processes = current_process;
+        Ok(())
+    }
+
+    pub fn monitor_processes(&mut self) {
+        println!("::: Process scanning starts :::\n");
+
+        loop {
+            let now = Instant::now();
+            if let Err(e) = self.scan_processes() {
+                println!("Error occured on scanning processes :: {:?}", e);
+            }
+            let elapsed = now.elapsed();
+            if elapsed < self.scan_interval {
+                sleep(self.scan_interval - elapsed)
+            }
+        }
+    }
 }
