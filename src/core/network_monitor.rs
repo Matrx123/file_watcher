b0d212f226454b7abd73257c999b3630643c4644
vipin joshi <vpnjsh55@gmail.com>
2025-08-11 14:06:42 +0530

    async implementation started

--- src/core/network_monitor.rs
+++ src/core/network_monitor.rs
@@ -2,401 +2,153 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::net::IpAddr;
 use std::sync::{Arc, Mutex};
-use std::thread;
 use std::time::{Duration, Instant};
 
-use pnet::datalink::{self, interfaces, Channel::Ethernet, NetworkInterface};
-use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
-use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::ipv4::Ipv4Packet;
-use pnet::packet::ipv6::Ipv6Packet;
-use pnet::packet::tcp::{TcpFlags, TcpPacket};
-use pnet::packet::udp::UdpPacket;
-use pnet::packet::Packet;
+use pnet::datalink::{interfaces, NetworkInterface};
+use tokio::sync::mpsc;
+use tokio::try_join;
 
-#[derive(Debug)]
-pub enum AlertLevel {
-    HIGH,
-}
-
-#[derive(Debug)]
-pub enum AlertType {
-    SYS,
-}
-
-#[derive(Debug)]
-pub struct Alert {
-    alert_level: AlertLevel,
-    alert_type: AlertType,
-    description: String,
-}
-
-pub trait AlertHandler: Send + Sync {
-    fn handle_alert(&self, alert: &Alert) -> Result<(), Box<dyn Error>>;
-}
-
-pub struct ConsoleAlertHandler;
-
-impl AlertHandler for ConsoleAlertHandler {
-    fn handle_alert(&self, alert: &Alert) -> Result<(), Box<dyn Error>> {
-        println!(
-            "========\nALERT\nLEVEL: {:?}\nALERT TYPE: {:?}\nDESCRIPTION: {:?}\n========",
-            alert.alert_level, alert.alert_type, alert.description
-        );
-        Ok(())
-    }
-}
-
-//console
-//file ops log file
-//notifications
-
-#[derive(Debug)]
-struct PacketDetail {
+pub struct PacketDetails {
     total_packets: u64,
     ipv4_packets: u64,
     ipv6_packets: u64,
     tcp_packets: u64,
-    udp_packets: u64,
+    upd_packets: u64,
     syn_packets: u64,
-    time_stamp: Instant,
+    last_update: Instant,
 }
 
-impl PacketDetail {
-    fn new() -> Self {
-        PacketDetail {
+impl PacketDetails {
+    pub fn new() -> Self {
+        PacketDetails {
             total_packets: 0,
             ipv4_packets: 0,
             ipv6_packets: 0,
             tcp_packets: 0,
-            udp_packets: 0,
+            upd_packets: 0,
             syn_packets: 0,
-            time_stamp: Instant::now(),
+            last_update: Instant::now(),
         }
     }
 }
 
-struct DetectAttacks {
-    syn_collection: HashMap<IpAddr, u32>,
-    packet_collection: HashMap<IpAddr, u32>,
-    udp_collection: HashMap<IpAddr, u32>,
+pub struct AttackDetector {
     syn_limit: u32,
     ddos_limit: u32,
     udp_limit: u32,
-    time_limit: Duration,
-    start_time_limit: Instant,
-    alert_handler: Vec<Box<dyn AlertHandler>>,
+    time_window: Duration,
+    syn_collection: HashMap<IpAddr, u32>,
+    udp_collection: HashMap<IpAddr, u32>,
+    tcp_collection: HashMap<IpAddr, u32>,
+    window_start: Instant,
 }
-
-impl DetectAttacks {
-    fn new() -> Self {
-        let mut det_attacks = DetectAttacks {
-            syn_collection: HashMap::new(),
-            packet_collection: HashMap::new(),
-            udp_collection: HashMap::new(),
-            syn_limit: 1000,
-            ddos_limit: 10000,
-            udp_limit: 10000,
-            time_limit: Duration::from_secs(10),
-            start_time_limit: Instant::now(),
-            alert_handler: Vec::new(),
-        };
-
-        //default alert_handler
-        det_attacks
-            .alert_handler
-            .push(Box::new(ConsoleAlertHandler));
-        det_attacks
-    }
-
-    fn send_alerts(&self, alert: &Alert) {
-        for item in &self.alert_handler {
-            if let Err(e) = item.handle_alert(alert) {
-                let alert = Alert {
-                    alert_level: AlertLevel::HIGH,
-                    alert_type: AlertType::SYS,
-                    description: format!("Error in sending the Alert!! {:?}", e),
-                };
-                self.send_alerts(&alert);
-            };
-        }
-    }
-
-    fn reset_values(&mut self) {
-        self.syn_collection.clear();
-        self.packet_collection.clear();
-        self.start_time_limit = Instant::now();
-    }
-
-    fn can_reset(&mut self) -> bool {
-        self.start_time_limit.elapsed() >= self.time_limit
-    }
-
-    fn is_udp_flood(&mut self, ip: IpAddr) -> bool {
-        let udp_count = self.udp_collection.entry(ip).or_insert(0);
-        *udp_count += 1;
-        if *udp_count > self.udp_limit {
-            let alert = Alert {
-                alert_level: AlertLevel::HIGH,
-                alert_type: AlertType::SYS,
-                description: format!(
-                    "UDP Flood detected from :: {:?} in time :: {:?}",
-                    ip, self.time_limit
-                ),
-            };
-            self.send_alerts(&alert);
-            return true;
-        }
-        false
-    }
-
-    fn is_syn_flood(&mut self, ip: IpAddr) -> bool {
-        let syn_count = self.syn_collection.entry(ip).or_insert(0);
-        *syn_count += 1;
-
-        if *syn_count > self.syn_limit {
-            let alert = Alert {
-                alert_level: AlertLevel::HIGH,
-                alert_type: AlertType::SYS,
-                description: format!(
-                    "Syn Flood detected from :: {:?} in time :: {:?}",
-                    ip, self.time_limit
-                ),
-            };
-            self.send_alerts(&alert);
-            return true;
-        }
-        false
-    }
-
-    fn is_ddos(&mut self, ip: IpAddr) -> bool {
-        let ddos_count = self.packet_collection.entry(ip).or_insert(0);
-        *ddos_count += 1;
-
-        if *ddos_count > self.ddos_limit {
-            let alert = Alert {
-                alert_level: AlertLevel::HIGH,
-                alert_type: AlertType::SYS,
-                description: format!(
-                    "Ddos Attack detected by IP :: {:?} in time :: {:?}",
-                    ip, self.time_limit
-                ),
-            };
-            self.send_alerts(&alert);
-            return true;
-        }
-        false
-    }
-}
-
-pub struct NetworkMonitor {
-    packet_stats: Arc<Mutex<PacketDetail>>,
-    detect_attack: Arc<Mutex<DetectAttacks>>,
-    alert_handler: Vec<Arc<Box<dyn AlertHandler>>>,
-}
-
-impl NetworkMonitor {
+impl AttackDetector {
     pub fn new() -> Self {
-        NetworkMonitor {
-            packet_stats: Arc::new(Mutex::new(PacketDetail::new())),
-            detect_attack: Arc::new(Mutex::new(DetectAttacks::new())),
-            alert_handler: vec![Arc::new(Box::new(ConsoleAlertHandler))],
+        Self {
+            syn_limit: 50,
+            ddos_limit: 5,
+            udp_limit: 50,
+            time_window: Duration::from_secs(7),
+            syn_collection: HashMap::new(),
+            udp_collection: HashMap::new(),
+            tcp_collection: HashMap::new(),
+            window_start: Instant::now(),
         }
     }
 
-    //interfaces > processed hte packets > processed the tcp/udp protocols > data
-    pub fn get_interface(&self) -> Option<NetworkInterface> {
+    pub fn reset_values(&mut self) {
+        self.syn_collection.clear();
+        self.tcp_collection.clear();
+        self.udp_collection.clear();
+        self.window_start = Instant::now();
+    }
+
+    async fn check_syn_flood(&mut self, ip: IpAddr) -> bool {
+        let count = self.syn_collection.entry(ip).or_insert(0);
+        *count += 1;
+
+        if *count > self.syn_limit {
+            println!("Possible syn flood detected  from {:?}!!", ip);
+            return true;
+        }
+        false
+    }
+
+    async fn check_udp_flood(&mut self, ip: IpAddr) -> bool {
+        let count = self.udp_collection.entry(ip).or_insert(0);
+        *count += 1;
+
+        if *count > self.udp_limit {
+            println!("Possible UDP flood detected from IP :: {:?}", ip);
+            return true;
+        }
+        false
+    }
+
+    async fn check_ddos(&mut self, ip: IpAddr) -> bool {
+        let count = self.tcp_collection.entry(ip).or_insert(0);
+        *count += 1;
+
+        if *count > self.ddos_limit {
+            println!("Possible DDOS attack detected from IP :: {:?}", ip);
+            return true;
+        }
+        false
+    }
+
+    fn should_reset(&self) -> bool {
+        self.window_start.elapsed() >= self.time_window
+    }
+}
+
+#[derive(Debug)]
+pub struct ProcessedPacket {
+    src_ip: IpAddr,
+    packet_type: PacketType,
+}
+
+#[derive(Debug)]
+enum PacketType {
+    Ipv4Tcp,
+    Ipv4Udp,
+    Ipv6Tcp,
+    Ipv6Udp,
+    Other,
+}
+
+pub struct AsyncNetworkMon {
+    packet_stats: Arc<Mutex<PacketDetails>>,
+    detect_attacks: Arc<Mutex<AttackDetector>>,
+}
+
+impl AsyncNetworkMon {
+    pub fn new() -> Self {
+        AsyncNetworkMon {
+            packet_stats: Arc::new(Mutex::new(PacketDetails::new())),
+            detect_attacks: Arc::new(Mutex::new(AttackDetector::new())),
+        }
+    }
+
+    pub fn get_interface() -> Option<NetworkInterface> {
         let interfaces = interfaces();
         interfaces
             .into_iter()
             .find(|iface| iface.name == "en0" && iface.is_up() && !iface.is_loopback())
     }
 
-    pub fn start_scanning_network(&self) -> Result<(), Box<dyn Error>> {
-        println!("Welcome to network scan center!!");
-        let mut monitor = NetworkMonitor::new();
-        monitor.show_alerts();
+    // pub async fn capture_packets(interface: NetworkInterface, sender: mpsc::UnboundedSender<>)
 
-        let interface = match monitor
-            .get_interface()
-            .ok_or("WARNING :: No interface found !!")
-        {
-            Ok(res) => res,
-            Err(e) => {
-                println!("Error in getting the interfaces :: {:?}", e);
-                panic!()
-            }
-        };
-
-        println!("::: INTERFACE FOUND :::");
-        println!("interface name :: {:?}", interface.name);
-        println!("interface description :: {:?}", interface.description);
-
-        monitor.start_monitoring(interface);
-
+    pub async fn start_network_monitor(
+        &self,
+        interface: NetworkInterface,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
+        let (sender, receiver) = mpsc::unbounded_channel::<Vec<u8>>();
+        try_join!(
+            self.capture_packets(interface, sender),
+            self.process_packet(receiver),
+            self.display_stats()
+        )?;
         Ok(())
     }
-
-    pub fn process_udp_packet(&self, ip: IpAddr) {
-        let mut stats = self.packet_stats.lock().unwrap();
-        stats.udp_packets += 1;
-        drop(stats);
-
-        let mut detector = self.detect_attack.lock().unwrap();
-        detector.is_udp_flood(ip);
-    }
-
-    pub fn process_tcp_packet(&self, tcp: &TcpPacket, ip: IpAddr) {
-        let mut stats = self.packet_stats.lock().unwrap();
-        stats.tcp_packets += 1;
-
-        if tcp.get_flags() & TcpFlags::SYN != 0 && tcp.get_flags() & TcpFlags::ACK == 0 {
-            stats.syn_packets += 1;
-            drop(stats);
-            let mut detector = self.detect_attack.lock().unwrap();
-            detector.is_syn_flood(ip);
-        }
-    }
-
-    pub fn process_ipv6_packets(&mut self, ipv6: &Ipv6Packet) {
-        let ip_addr = IpAddr::V6(ipv6.get_source());
-        {
-            let mut stats = self.packet_stats.lock().unwrap();
-            stats.total_packets += 1;
-            stats.ipv4_packets += 1;
-            stats.time_stamp = Instant::now();
-        }
-
-        {
-            //ddos attacks
-            let mut detector = self.detect_attack.lock().unwrap();
-            if detector.can_reset() {
-                detector.reset_values();
-            }
-            detector.is_ddos(ip_addr);
-        }
-        match ipv6.get_next_header() {
-            IpNextHeaderProtocols::Tcp => {
-                if let Some(tcp) = TcpPacket::new(ipv6.payload()) {
-                    self.process_tcp_packet(&tcp, ip_addr);
-                }
-            }
-            IpNextHeaderProtocols::Udp => {
-                if let Some(_udp) = UdpPacket::new(ipv6.payload()) {
-                    self.process_udp_packet(ip_addr);
-                }
-            }
-            _ => {}
-        }
-    }
-
-    pub fn process_ipv4_packets(&mut self, ipv4: &Ipv4Packet) {
-        let ip_addr = IpAddr::V4(ipv4.get_source());
-        {
-            let mut stats = self.packet_stats.lock().unwrap();
-            stats.total_packets += 1;
-            stats.ipv4_packets += 1;
-            stats.time_stamp = Instant::now();
-        }
-
-        {
-            //ddos attacks
-            let mut detector = self.detect_attack.lock().unwrap();
-            if detector.can_reset() {
-                detector.reset_values();
-            }
-            detector.is_ddos(ip_addr);
-        }
-
-        match ipv4.get_next_level_protocol() {
-            IpNextHeaderProtocols::Tcp => {
-                if let Some(tcp) = TcpPacket::new(ipv4.payload()) {
-                    self.process_tcp_packet(&tcp, ip_addr);
-                }
-            }
-            IpNextHeaderProtocols::Udp => {
-                if let Some(_udp) = UdpPacket::new(ipv4.payload()) {
-                    self.process_udp_packet(ip_addr);
-                }
-            }
-            _ => {}
-        }
-    }
-
-    pub fn process_packets(&mut self, packet: &[u8]) {
-        if let Some(ethernet_packet) = EthernetPacket::new(packet) {
-            match ethernet_packet.get_ethertype() {
-                EtherTypes::Ipv4 => {
-                    if let Some(ipv4) = Ipv4Packet::new(ethernet_packet.payload()) {
-                        self.process_ipv4_packets(&ipv4);
-                    }
-                }
-                EtherTypes::Ipv6 => {
-                    if let Some(ipv6) = Ipv6Packet::new(ethernet_packet.payload()) {
-                        self.process_ipv6_packets(&ipv6);
-                    }
-                }
-                _ => {}
-            }
-        }
-    }
-
-    pub fn start_monitoring(&mut self, interface: NetworkInterface) {
-        println!("Network scanning in progress!!");
-        println!("press ctrl+c to stop!!");
-
-        let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
-            Ok(Ethernet(tx, rx)) => (tx, rx),
-            Ok(_) => panic!("Unhandled channel type."),
-            Err(e) => panic!("An error occurred: {}", e),
-        };
-
-        loop {
-            match rx.next() {
-                Ok(packet) => self.process_packets(packet),
-                Err(e) => {
-                    eprintln!("Error in receiving packets !! {}", e);
-                }
-            }
-        }
-    }
-
-    pub fn show_alerts(&self) {
-        let cloned_stat = Arc::clone(&self.packet_stats);
-        let cloned_alert_handler = self.alert_handler.clone();
-        thread::spawn(move || loop {
-            thread::sleep(Duration::from_secs(5));
-            let stats = cloned_stat.lock().unwrap();
-
-            let alert = Alert {
-                alert_level: AlertLevel::HIGH,
-                alert_type: AlertType::SYS,
-                description: format!(
-                    "\n Statistics from last 5 Seconds :: \n\
-                Total Packets :: {}\n\
-                IPV4 Packets :: {}\n\
-                IPV6 Packets :: {}\n\
-                TCP Packets :: {}\n\
-                UDP Packets :: {}\n\
-                SYN Packets :: {}\n\
-                {}\n
-                ",
-                    stats.total_packets,
-                    stats.ipv4_packets,
-                    stats.ipv6_packets,
-                    stats.tcp_packets,
-                    stats.udp_packets,
-                    stats.syn_packets,
-                    "--".repeat(30)
-                ),
-            };
-
-            for item in &cloned_alert_handler {
-                if let Err(e) = item.handle_alert(&alert) {
-                    //raise an alert
-                    eprintln!("Error is here!!!! : {:?}", e);
-                }
-            }
-        });
-    }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::process;
 use std::time::Duration;
 mod core;
 
-fn main() {
-    let mut file_watcher = core::watcher::FileWatcher::new();
-    let mut process_monitor =
-        core::process_watcher::ProcessMonitor::new(Duration::new(20, 0), 90.00, 90.00, 5);
+#[tokio::main]
+async fn main() {
+    // let mut file_watcher = core::watcher::FileWatcher::new();
+    // let mut process_monitor =
+    //     core::process_watcher::ProcessMonitor::new(Duration::new(20, 0), 90.00, 90.00, 5);
     let network_monitor = core::network_monitor::NetworkMonitor::new();
-    eprintln!("Status :: {:?}", file_watcher.check_status());
+    // eprintln!("Status :: {:?}", file_watcher.check_status());
     println!("::: WELCOME TO SCANGUARD :::\n\n +++ Enter the TARGET TYPE to watch!! +++ \n\n");
 
     // if let Err(e) = file_watcher.hash_string("/Users/vipin/Documents/test_folder/virus.txt") {
@@ -34,56 +35,56 @@ fn main() {
                         Err(_) => {
                             println!("user entered :: {:?}", user_entry.trim());
                             match user_entry.trim() {
-                                "FILE" | "file" => {
-                                    println!("Enter a file or directory to monitor !!\n");
-                                    let mut user_path = String::new();
-                                    match stdin().read_line(&mut user_path) {
-                                        Ok(_) => {
-                                            if user_path.trim().is_empty() {
-                                                println!(
-                                                    "Please enter a File or Directory to watch \n"
-                                                );
-                                                continue;
-                                            }
-                                            if Path::new(user_path.trim()).exists() {
-                                                println!(
-                                                    "Watching a Directory :: {:?}",
-                                                    user_path.trim()
-                                                );
-                                                if let Err(e) = file_watcher
-                                                    .start_monitoring(user_path.as_str().trim())
-                                                {
-                                                    eprintln!(
-                                                        "Error occured, making an Exit !! {:?}\n",
-                                                        e
-                                                    );
-                                                    break;
-                                                }
-                                            } else {
-                                                println!("Directory or file does not existes!! \n");
-                                                continue;
-                                            }
-                                        }
-                                        Err(e) => {
-                                            eprintln!(
-                                                "\nError in reading the input path :: {:?}",
-                                                e
-                                            );
-                                            process::exit(0);
-                                        }
-                                    }
-                                }
-                                "PROCESS" | "process" => {
-                                    process_monitor.monitor_processes();
-                                    break;
-                                }
+                                // "FILE" | "file" => {
+                                //     println!("Enter a file or directory to monitor !!\n");
+                                //     let mut user_path = String::new();
+                                //     match stdin().read_line(&mut user_path) {
+                                //         Ok(_) => {
+                                //             if user_path.trim().is_empty() {
+                                //                 println!(
+                                //                     "Please enter a File or Directory to watch \n"
+                                //                 );
+                                //                 continue;
+                                //             }
+                                //             if Path::new(user_path.trim()).exists() {
+                                //                 println!(
+                                //                     "Watching a Directory :: {:?}",
+                                //                     user_path.trim()
+                                //                 );
+                                //                 if let Err(e) = file_watcher
+                                //                     .start_monitoring(user_path.as_str().trim())
+                                //                 {
+                                //                     eprintln!(
+                                //                         "Error occured, making an Exit !! {:?}\n",
+                                //                         e
+                                //                     );
+                                //                     break;
+                                //                 }
+                                //             } else {
+                                //                 println!("Directory or file does not existes!! \n");
+                                //                 continue;
+                                //             }
+                                //         }
+                                //         Err(e) => {
+                                //             eprintln!(
+                                //                 "\nError in reading the input path :: {:?}",
+                                //                 e
+                                //             );
+                                //             process::exit(0);
+                                //         }
+                                //     }
+                                // }
+                                // "PROCESS" | "process" => {
+                                //     process_monitor.monitor_processes();
+                                //     break;
+                                // }
                                 "NETWORK" | "network" => {
                                     // let uid = getuid();
                                     // if !uid {
                                     //     std::process::exit(1);
                                     // }
                                     // network_monitor.show_alerts();
-                                    network_monitor.start_scanning_network().unwrap();
+                                    // network_monitor.start_scanning_network().unwrap();
                                     break;
                                 }
                                 _ => {
